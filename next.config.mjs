baf8a018cad3e31c923efdefaf2f0b79bbb69bef
Nimra Bilal <yoyo123000001@gmail.com>
2026-01-31 02:00:43 +0500

    new2

--- next.config.mjs
+++ next.config.mjs
@@ -20,31 +20,11 @@
 // module.exports = nextConfig;
 
 
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     domains: ['res.cloudinary.com'], // simple and works
-//   },
-// };
-
-// module.exports = nextConfig;
-
-
-
-// next.config.js
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
-      unoptimized: true, 
-    // For Next.js 13+
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        port: '',
-        pathname: '/**', // Allows ALL Cloudinary images
-      },
-    ],
-    // Also add domains for backward compatibility
-    domains: ['res.cloudinary.com'],
+    domains: ['res.cloudinary.com'], // simple and works
   },
-}
+};
+
+module.exports = nextConfig;
